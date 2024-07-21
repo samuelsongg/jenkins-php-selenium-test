6640cbefb9654bb5f5313ce9998830caf6570ed9
samuelsongg <samuelsongg@gmail.com>
2024-07-21 21:38:50 +0800

    Update path

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-# docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\Samuel\\Projects\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
-docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/Samuel/Projects/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\Samuel\\Projects\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
